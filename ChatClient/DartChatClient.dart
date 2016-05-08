bb8758f26ee77340819b3cd3d6b66f7dd1c3933a
Priscila Gouveia <priscilaggouveia@gmail.com>
2016-05-08 16:02:23 -0300

    Cliente Dart atualizado

--- ChatClient/DartChatClient.dart
+++ ChatClient/DartChatClient.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 import 'dart:collection';
+import "dart:async";
 
 Socket server;
 String credential; //email
 String id;
+bool isLog = false;
+bool isSignin = false;
 
 void handleServer(Socket socket) {
   Socket.connect('127.0.0.1', 10998).then((socket) {
@@ -35,17 +38,22 @@ void signIn() {
   server.write("signin:" + credential + ":" + name);
   server.listen((data) {
     id = new String.fromCharCodes(data).trim();
+    print("Você foi cadastrado com o id " + id);
   });
+  isSignin = true;
 }
 
-//<login>:<id>
+//<login>:<credential>
 void login() {
-  server.write("login:" + id);
+  server.write("login:" + credential);
+  isLog = true;
+  print("Você está logado!");
 }
 
-//<logout>:<id>
+//<logout>:<credential>
 void logout() {
-  server.write("logout:" + id);
+  server.write("logout:" + credential);
+  isLog = false;
 }
 
 //<getmessages>:
@@ -105,8 +113,53 @@ void getRegisteredGroups() {
   response.forEach((r) => print(r));
 }
 
-void main() {
-//  Socket.connect('127.0.0.1', 10998).then((socket) {
-//    handleServer(socket);
-//  });
+void doLogin(String cod) {
+  while (!isLog) {
+    switch (cod) {
+      case "login":
+        {
+          if (isSignin) {
+            login();
+          } else {
+            print("Primeiro você deve realizar o seu cadastro!");
+            signIn();
+          }
+        }
+        break;
+      case "cadastro":
+        signIn();
+        break;
+      default:
+        {
+          print("O que você gostaria de fazer?\n- login\n- cadastro");
+          cod = stdin.readLineSync();
+        }
+    }
+  }
+}
+
+Future runThreadMessages() async {
+  while (true) {
+    getMessages();
+  }
+}
+
+Future runThreadPublish() async {
+  while (true) {
+    publish();
+  }
+}
+
+Future main() async {
+  Socket.connect('127.0.0.1', 10998).then((socket) {
+    handleServer(socket);
+  });
+
+  print("O que você gostaria de fazer?\n- login\n- cadastro");
+  String cod = stdin.readLineSync();
+  doLogin(cod);
+
+  await runThreadPublish();
+
+  await runThreadMessages();
 }
